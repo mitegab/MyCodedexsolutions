15cef85e7fa50620e88d337aeb782df659910e87
mitegab <104057957+mitegab@users.noreply.github.com>
2024-02-29 03:42:45 -0500

    print hello world

---
+++ python/HellWorld/hello_world.py
@@ -0,0 +1,32 @@
+"""
+
+
+Hello World
+# Print
+In Python, the print() function is used to tell a computer to "talk." This is something we are going to use a lot.
+
+
+
+The message we want to display should be inside the parentheses and surrounded by quotes. They can be double quotes " or single quotes ', but the opening and closing quote marks have to be the same.
+
+This is an example of a print() function:
+
+print('👋 Howdy')
+
+In the example above, we instructed our program to print a message. This resulting text that is printed to the screen is referred to as the output.
+
+The output of this example program would be:
+
+👋 Howdy
+
+Instructions
+Create a new hello_world.py program.
+
+On a new line, use print() to output the message "Hello World!"
+
+And then run the program.
+
+Were you able to print "Hello World!" in the terminal
+
+"""
+print("Hello World!")
